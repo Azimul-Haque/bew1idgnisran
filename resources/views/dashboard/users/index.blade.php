2ee0392614bbc2b4dabaebb9e43ca2d6cebd0de5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-27 03:25:03 +0600

    Commit Sat 12/27/2025  3:25:03.28 13479

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -450,7 +450,6 @@
 
                       <!-- Designation and Password Fields -->
                       <div class="row">
-                          
                           <div class="col-md-6">
                               <div class="input-group mb-3">
                                   <input type="password"
