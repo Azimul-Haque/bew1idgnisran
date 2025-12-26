2fd1d202d8c27a54ababb83c1c13c6c29e6e602a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-27 02:46:26 +0600

    Commit Sat 12/27/2025  2:46:26.32 18514

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,11 +62,11 @@
 
         <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item" data-toggle="tooltip" data-original-title="ভিডিও টিউটোরিয়াল">
+          {{-- <li class="nav-item" data-toggle="tooltip" data-original-title="ভিডিও টিউটোরিয়াল">
             <a class="nav-link" href="{{ route('dashboard.video.tutorials') }}">
               <i class="fab fa-youtube"></i>
             </a>
-          </li>
+          </li> --}}
           <li class="nav-item" data-toggle="tooltip" data-original-title="ওয়েবসাইট দেখুন">
             <a class="nav-link" href="{{ route('index.index') }}"  target="_blank">
               <i class="fas fa-globe"></i>
