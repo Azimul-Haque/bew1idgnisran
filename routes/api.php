0a68cd9ed9832a4ca7594ad83fa217fe0da68097
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-27 05:03:41 +0600

    Commit Sat 12/27/2025  5:03:41.30 20753

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', 'APIController@login')->name('api.login');
 Route::post('programs/store', 'APIController@storeProgram')->name('api.store.program');
-Route::post('programs/list', 'APIController@storeProgram')->name('api.store.program');
+Route::post('programs/list', 'APIController@getPrograms')->name('api.store.program');
 Route::get('/programs', [ProgramController::class, 'index']);
 
 
