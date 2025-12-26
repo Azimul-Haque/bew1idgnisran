2a1ca257202042ea99b2b0766c1a572eaeb2b985
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-27 03:55:33 +0600

    Commit Sat 12/27/2025  3:55:33.21 5652

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', 'APIController@login')->name('api.login');
+
+
+
+
+
+
+
+
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
