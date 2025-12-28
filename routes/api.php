99f5bb1e7f392aac9498bd581483796472d292c1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-28 23:16:18 +0600

    Commit Sun 12/28/2025 23:16:18.26 24997

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,8 @@ Route::post('notices/store', 'APIController@storeNotice')->name('api.store.notic
 Route::post('notices/update/{id}', 'APIController@updateNotice')->name('api.update.notice');
 Route::delete('notices/delete/{id}', 'APIController@deleteNotice')->name('api.delete.notice');
 
+Route::delete('units', 'APIController@deleteNotice')->name('api.delete.notice');
+
 
 
 
