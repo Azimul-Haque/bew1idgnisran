ea545b92fd62400349a24b3c70356baece49f50e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-28 23:38:58 +0600

    Commit Sun 12/28/2025 23:38:58.30 28035

--- database/migrations/2025_12_28_233837_create_units_table.php
+++ database/migrations/2025_12_28_233837_create_units_table.php
@@ -15,6 +15,7 @@ class CreateUnitsTable extends Migration
     {
         Schema::create('units', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique(); // ওয়ার্ড বা ইউনিটের নাম
             $table->timestamps();
         });
     }
