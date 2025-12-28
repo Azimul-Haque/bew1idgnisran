3f96f659230d9475f6257345e75371e83e633953
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-28 23:44:59 +0600

    Commit Sun 12/28/2025 23:44:59.22 5278

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,10 @@ Route::post('notices/update/{id}', 'APIController@updateNotice')->name('api.upda
 Route::delete('notices/delete/{id}', 'APIController@deleteNotice')->name('api.delete.notice');
 
 Route::get('units', 'APIController@getUnits')->name('api.get.units');
-Route::get('notices/list', 'APIController@getNotices')->name('api.list.notices');
-Route::post('notices/store', 'APIController@storeNotice')->name('api.store.notice');
-Route::post('notices/update/{id}', 'APIController@updateNotice')->name('api.update.notice');
-Route::delete('notices/delete/{id}', 'APIController@deleteNotice')->name('api.delete.notice');
+Route::get('leaders/list', 'APIController@getNotices')->name('api.list.notices');
+Route::post('leaders/store', 'APIController@storeNotice')->name('api.store.notice');
+Route::post('leaders/update/{id}', 'APIController@updateNotice')->name('api.update.notice');
+Route::delete('leaders/delete/{id}', 'APIController@deleteNotice')->name('api.delete.notice');
 
 
 
