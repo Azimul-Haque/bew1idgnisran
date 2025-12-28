c2a2027d8be3f26e8e2ca502964ee9c5b3c525d7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-28 23:39:05 +0600

    Commit Sun 12/28/2025 23:39:05.27 15381

--- database/migrations/2025_12_28_233759_create_leaders_table.php
+++ database/migrations/2025_12_28_233759_create_leaders_table.php
@@ -15,6 +15,11 @@ class CreateLeadersTable extends Migration
     {
         Schema::create('leaders', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('designation');
+            $table->string('unit'); // ড্রপডাউন থেকে আসা ভ্যালু
+            $table->integer('serial_priority')->default(1);
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
