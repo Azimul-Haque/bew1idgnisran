8fe6491b0575bd8aec0c25096d7f8f55dd855a2b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-01 04:02:38 +0600

    Commit Thu 01/01/2026  4:02:38.28 32143

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -100,7 +100,8 @@ class APIController extends Controller
 
         $program->save();
 
-        Cache::forget('programs_list'); // ক্যাশ মুছে ফেলা হলো
+        Cache::forget('programs_list');
+        Cache::forget('admin_stats');
 
         return response()->json([
             'status' => 'success',
