de17a74fb54909a2c2b76ce75a2110ce27aa0b7b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-11 02:39:40 +0600

    Commit Sun 01/11/2026  2:39:40.31 12081

--- app/Program.php
+++ app/Program.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Program extends Model
 {
-    public function programatts() {
-      return $this->hasMany('App\Programatt');
+    public function attendances() {
+        // সরাসরি ক্লাস ব্যবহার করা স্ট্রিং ব্যবহারের চেয়ে ভালো
+        return $this->hasMany(\App\Models\Programatt::class, 'program_id');
     }
 }
