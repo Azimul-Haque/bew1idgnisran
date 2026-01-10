63c6b8cf39b3cffe82e0cb32dd06330449091054
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-11 02:40:05 +0600

    Commit Sun 01/11/2026  2:40:05.25 3177

--- app/Program.php
+++ app/Program.php
@@ -8,6 +8,6 @@ class Program extends Model
 {
     public function attendances() {
         // সরাসরি ক্লাস ব্যবহার করা স্ট্রিং ব্যবহারের চেয়ে ভালো
-        return $this->hasMany(\App\Models\Programatt::class, 'program_id');
+        return $this->hasMany('App\Programatt', 'program_id');
     }
 }
