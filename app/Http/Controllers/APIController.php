808df271fbd45e6427a7c1cb0d3192220a52e292
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-13 03:42:50 +0600

    Commit Tue 01/13/2026  3:42:50.29 8813

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -680,7 +680,6 @@ class APIController extends Controller
             $voters = Voter::where('area_name', $area)
                 ->where(function($q) use ($search) {
                     $q->where('name', 'LIKE', "%$search%")
-                      ->orWhere('area_name', 'LIKE', "$search%");
                       ->orWhere('voter_no', 'LIKE', "$search%");
                       ->orWhere('father', 'LIKE', "%$search%");
                       ->orWhere('mother', 'LIKE', "%$search%");
