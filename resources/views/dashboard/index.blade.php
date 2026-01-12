de240073074522cc8030f9602f83b98e9fb97c38
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-13 02:52:03 +0600

    Commit Tue 01/13/2026  2:52:03.26 19775

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -137,7 +137,7 @@
                 <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header bg-success">
-                    <h5 class="modal-title" id="clearQueryCacheModalLabel">কোয়েরি ক্যাশ ক্লিয়ার</h5>
+                    <h5 class="modal-title" id="clearQueryCacheModalLabel">CSV ফাইল আপলোড</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
