9f2db9bab1962d8d9fe39ff2881640ba3931b084
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-13 02:40:45 +0600

    Commit Tue 01/13/2026  2:40:45.21 7155

--- database/migrations/2026_01_13_023758_create_voters_table.php
+++ database/migrations/2026_01_13_023758_create_voters_table.php
@@ -15,6 +15,24 @@ class CreateVotersTable extends Migration
     {
         Schema::create('voters', function (Blueprint $table) {
             $table->id();
+            // ইউনিক ইনডেক্স সরিয়ে দিয়ে সাধারণ ইনডেক্স করা হয়েছে
+            $table->string('voter_no', 20)->index(); 
+            $table->string('area_no', 15)->index();
+            
+            $table->string('serial', 15)->nullable();
+            $table->string('gender', 10)->nullable();
+            
+            $table->string('name');
+            $table->string('father')->nullable();
+            $table->string('mother')->nullable();
+            $table->string('dob', 30)->nullable();
+            $table->string('occupation')->nullable();
+            $table->text('address')->nullable();
+
+            $table->string('union_municipality')->nullable();
+            $table->string('ward', 10)->nullable();
+            $table->string('area_name')->nullable();
+
             $table->timestamps();
         });
     }
