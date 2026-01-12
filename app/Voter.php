6c557bba9b5c159b6aca11efad33eae7164b091e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-13 02:43:36 +0600

    Commit Tue 01/13/2026  2:43:36.27 28698

--- app/Voter.php
+++ app/Voter.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Voter extends Model
 {
-    //
+    protected $fillable = [
+        'union_municipality', 'ward', 'area_name', 'area_no', 
+        'gender', 'serial', 'voter_no', 'name', 
+        'father', 'mother', 'dob', 'occupation', 'address'
+    ];
 }
