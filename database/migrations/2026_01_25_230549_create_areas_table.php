bc37a68b3d4d45d8b1ac50b25d19656ca2496632
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-25 23:09:57 +0600

    Commit Sun 01/25/2026 23:09:57.24 27534

--- database/migrations/2026_01_25_230549_create_areas_table.php
+++ database/migrations/2026_01_25_230549_create_areas_table.php
@@ -15,6 +15,20 @@ class CreateAreasTable extends Migration
     {
         Schema::create('areas', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 40);
+            $table->string('code', 6)->index();
+            $table->string('word_no', 4);
+            $table->foreignId('council_id')->nullable()->constrained('councils')->onDelete('set null');
+            $table->unsignedSmallInteger('sub_district_id')->nullable();
+            $table->unsignedSmallInteger('district_id')->nullable();
+            $table->unsignedSmallInteger('constituency_id');
+            $table->unsignedSmallInteger('male')->default(0);
+            $table->unsignedSmallInteger('migration_male')->default(0);
+            $table->unsignedSmallInteger('female')->default(0);
+            $table->unsignedSmallInteger('migration_female')->default(0);
+            $table->unsignedSmallInteger('hijra')->default(0);
+            $table->unsignedSmallInteger('migration_hijra')->default(0);
+            $table->date('published')->nullable();
             $table->timestamps();
         });
     }
