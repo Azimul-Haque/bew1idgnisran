c7855311dbc6b61bbff635bd30036dd15495b798
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-25 23:58:56 +0600

    Commit Sun 01/25/2026 23:58:56.30 16492

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,7 @@ Route::get('register/citizen', 'IndexController@getCitizenRegister')->name('regi
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
 Route::get('/dashboard/clear/query/cache', 'DashboardController@clearQueryCache')->name('dashboard.clearquerycache');
 Route::post('/dashboard/upload/csv', 'DashboardController@uploadCSV')->name('dashboard.uploadcsv');
+Route::post('/dashboard/areas/json', 'DashboardController@makeAreasJson')->name('dashboard.areas/json');
 
 Route::get('/dashboard/users', 'DashboardController@getUsers')->name('dashboard.users');
 Route::get('/dashboard/users/sort', 'DashboardController@getUsersSort')->name('dashboard.userssort');
