64ee10a39049d9e6168eea38c75bce210ed78024
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-26 00:41:20 +0600

    Commit Mon 01/26/2026  0:41:20.21 24926

--- database/migrations/2026_01_13_023758_create_voters_table.php
+++ database/migrations/2026_01_13_023758_create_voters_table.php
@@ -40,6 +40,8 @@ class CreateVotersTable extends Migration
 
             // ৩. শুধু সেন্টার ভিত্তিক কুয়েরির জন্য
             $table->index('center_id');
+
+            $table->index('voter_id');
             
             $table->timestamps();
         });
