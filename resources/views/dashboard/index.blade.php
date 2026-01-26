b701d3fce3c74e0a15a7a0f7b4ac945dd36177e8
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 01:53:34 +0600

    Commit Tue 01/27/2026  1:53:34.33 17476

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,160 +14,156 @@
     </ol>
   @endsection
     <div class="container-fluid">
-      @if(Auth::user()->role == 'admin')
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h4>{{ bangla($totalusers) }} জন</h4>
+      <div class="row">
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h4>{{ bangla($totalusers) }} জন</h4>
 
-                <p>মোট ব্যবহারকারী</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="{{ route('dashboard.users') }}" class="small-box-footer">ব্যবহারকারী পাতা <i class="fas fa-arrow-circle-right"></i></a>
+              <p>মোট ব্যবহারকারী</p>
             </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="{{ route('dashboard.users') }}" class="small-box-footer">ব্যবহারকারী পাতা <i class="fas fa-arrow-circle-right"></i></a>
           </div>
-          
-          {{-- <div class="col-lg-3 col-6">
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h4>{{ $totalmonthlypayment->totalamount ? ceil($totalmonthlypayment->totalamount) : 0 }}<sup style="font-size: 20px">৳</sup></h4>
-
-                <p>মাসিক আয় ({{ date('F Y') }})</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="{{ route('dashboard.payments') }}" class="small-box-footer">মাসিক আয় পাতা <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div> --}}
-          <!-- ./col -->
-          {{-- <div class="col-lg-3 col-6">
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h4>
-                  {{ $totalexamsattendedtoday }} বার
-                </h4>
-
-                <p>আজ সার্টিফিকেট প্রদান ({{ date("F d, Y") }})</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="#!" class="small-box-footer">পরীক্ষার্থী তালিকা <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div> --}}
-          <!-- ./col -->
-          {{-- <div class="col-lg-3 col-6">
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h4>{{ $totallocaloffices }}</h4>
-
-                <p>মোট ইউনিয়ন/পৌরসভা</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="{{ route('dashboard.local-offices') }}" class="small-box-footer">ব্যবহারকারীগণ <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div> --}}
         </div>
-        {{-- <div class="row">
-          <div class="col-md-6">
-            <a href="" class="info-box mb-3">
-              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-coins"></i></span>
+        
+        {{-- <div class="col-lg-3 col-6">
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h4>{{ $totalmonthlypayment->totalamount ? ceil($totalmonthlypayment->totalamount) : 0 }}<sup style="font-size: 20px">৳</sup></h4>
 
-              <div class="info-box-content">
-                <span class="info-box-text">দৈনিক জমা</span>
-                <span class="info-box-number">৳ {{ 0 }}</span>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-6">
-            <a href="" class="info-box mb-3">
-              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-hand-holding-usd"></i></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">দৈনিক খরচ</span>
-                <span class="info-box-number">৳ {{ 0 }}</span>
-              </div>
-            </a>
+              <p>মাসিক আয় ({{ date('F Y') }})</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="{{ route('dashboard.payments') }}" class="small-box-footer">মাসিক আয় পাতা <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div> --}}
-        <div class="row">
-          <div class="col-md-6">
-            <button class="btn btn-warning" data-toggle="modal" data-target="#clearQueryCacheModal">
-              <i class="fas fa-tools"></i> সকল কোয়েরি ক্যাশ (API) ক্লিয়ার করুন
-            </button>
-            {{-- Modal Code --}}
-            {{-- Modal Code --}}
-            <!-- Modal -->
-            <div class="modal fade" id="clearQueryCacheModal" tabindex="-1" role="dialog" aria-labelledby="clearQueryCacheModalLabel" aria-hidden="true" data-backdrop="static">
-                <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header bg-warning">
-                    <h5 class="modal-title" id="clearQueryCacheModalLabel">কোয়েরি ক্যাশ ক্লিয়ার</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    </div>
-                    <div class="modal-body">
-                      আপনি কি নিশ্চিতভাবে সকল কোয়েরি ক্যাশ ক্লিয়ার করতে চান?<br/>
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                    <a href="{{ route('dashboard.clearquerycache') }}" class="btn btn-warning">ক্যাশ ক্লিয়ার করুন</a>
-                    </div>
-                </div>
-                </div>
-            </div>
-            {{-- Modal Code --}}
-            {{-- Modal Code --}}
+        <!-- ./col -->
+        {{-- <div class="col-lg-3 col-6">
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h4>
+                {{ $totalexamsattendedtoday }} বার
+              </h4>
 
-            {{-- <button class="btn btn-success" data-toggle="modal" data-target="#uploadCSVFileModal">
-              <i class="fas fa-files"></i> CSV ফাইল আপলোড
-            </button> --}}
-            {{-- Modal Code --}}
-            {{-- Modal Code --}}
-            <!-- Modal -->
-            <div class="modal fade" id="uploadCSVFileModal" tabindex="-1" role="dialog" aria-labelledby="uploadCSVFileModalLabel" aria-hidden="true" data-backdrop="static">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                      <div class="modal-header bg-success">
-                        <h5 class="modal-title" id="clearQueryCacheModalLabel">CSV ফাইল আপলোড</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <form action="{{ route('dashboard.uploadcsv') }}" method="POST" enctype="multipart/form-data">
-                          <div class="modal-body">
-                              @csrf
-                              <div class="form-group mb-3">
-                                  <label class="font-weight-bold">সিএসভি ফাইলগুলো একসাথে সিলেক্ট করুন (১০-২০টি):</label>
-                                  <input type="file" name="csv_files[]" class="form-control" multiple required>
-                                  <small class="text-muted">আপনি কিবোর্ডের Ctrl চেপে ধরে একাধিক ফাইল সিলেক্ট করতে পারেন।</small>
-                              </div>
-                          </div>
-                          <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                              <button type="submit" class="btn btn-primary">সবগুলো আপলোড শুরু করুন</button>
-                          </div>
-                      </form>
-                  </div>
-                </div>
+              <p>আজ সার্টিফিকেট প্রদান ({{ date("F d, Y") }})</p>
             </div>
-            {{-- Modal Code --}}
-            {{-- Modal Code --}}
-            <br/>
-            <br/>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#!" class="small-box-footer">পরীক্ষার্থী তালিকা <i class="fas fa-arrow-circle-right"></i></a>
           </div>
+        </div> --}}
+        <!-- ./col -->
+        {{-- <div class="col-lg-3 col-6">
+          <div class="small-box bg-danger">
+            <div class="inner">
+              <h4>{{ $totallocaloffices }}</h4>
+
+              <p>মোট ইউনিয়ন/পৌরসভা</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="{{ route('dashboard.local-offices') }}" class="small-box-footer">ব্যবহারকারীগণ <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div> --}}
+      </div>
+      {{-- <div class="row">
+        <div class="col-md-6">
+          <a href="" class="info-box mb-3">
+            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-coins"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">দৈনিক জমা</span>
+              <span class="info-box-number">৳ {{ 0 }}</span>
+            </div>
+          </a>
         </div>
-      @elseif(Auth::user()->role == 'manager')
-        @include('partials._manager_dashboard')
-      @endif
+        <div class="col-md-6">
+          <a href="" class="info-box mb-3">
+            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-hand-holding-usd"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">দৈনিক খরচ</span>
+              <span class="info-box-number">৳ {{ 0 }}</span>
+            </div>
+          </a>
+        </div>
+      </div> --}}
+      <div class="row">
+        <div class="col-md-6">
+          <button class="btn btn-warning" data-toggle="modal" data-target="#clearQueryCacheModal">
+            <i class="fas fa-tools"></i> সকল কোয়েরি ক্যাশ (API) ক্লিয়ার করুন
+          </button>
+          {{-- Modal Code --}}
+          {{-- Modal Code --}}
+          <!-- Modal -->
+          <div class="modal fade" id="clearQueryCacheModal" tabindex="-1" role="dialog" aria-labelledby="clearQueryCacheModalLabel" aria-hidden="true" data-backdrop="static">
+              <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                  <div class="modal-header bg-warning">
+                  <h5 class="modal-title" id="clearQueryCacheModalLabel">কোয়েরি ক্যাশ ক্লিয়ার</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+                  </div>
+                  <div class="modal-body">
+                    আপনি কি নিশ্চিতভাবে সকল কোয়েরি ক্যাশ ক্লিয়ার করতে চান?<br/>
+                  </div>
+                  <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                  <a href="{{ route('dashboard.clearquerycache') }}" class="btn btn-warning">ক্যাশ ক্লিয়ার করুন</a>
+                  </div>
+              </div>
+              </div>
+          </div>
+          {{-- Modal Code --}}
+          {{-- Modal Code --}}
+
+          {{-- <button class="btn btn-success" data-toggle="modal" data-target="#uploadCSVFileModal">
+            <i class="fas fa-files"></i> CSV ফাইল আপলোড
+          </button> --}}
+          {{-- Modal Code --}}
+          {{-- Modal Code --}}
+          <!-- Modal -->
+          <div class="modal fade" id="uploadCSVFileModal" tabindex="-1" role="dialog" aria-labelledby="uploadCSVFileModalLabel" aria-hidden="true" data-backdrop="static">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header bg-success">
+                      <h5 class="modal-title" id="clearQueryCacheModalLabel">CSV ফাইল আপলোড</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <form action="{{ route('dashboard.uploadcsv') }}" method="POST" enctype="multipart/form-data">
+                        <div class="modal-body">
+                            @csrf
+                            <div class="form-group mb-3">
+                                <label class="font-weight-bold">সিএসভি ফাইলগুলো একসাথে সিলেক্ট করুন (১০-২০টি):</label>
+                                <input type="file" name="csv_files[]" class="form-control" multiple required>
+                                <small class="text-muted">আপনি কিবোর্ডের Ctrl চেপে ধরে একাধিক ফাইল সিলেক্ট করতে পারেন।</small>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                            <button type="submit" class="btn btn-primary">সবগুলো আপলোড শুরু করুন</button>
+                        </div>
+                    </form>
+                </div>
+              </div>
+          </div>
+          {{-- Modal Code --}}
+          {{-- Modal Code --}}
+          <br/>
+          <br/>
+        </div>
+      </div>
     </div>
 @endsection
 
