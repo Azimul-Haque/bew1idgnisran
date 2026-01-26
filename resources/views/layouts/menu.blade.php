e768ddb988f1798d395c3b73a5c887f1ee8a3880
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 00:20:42 +0600

    Commit Tue 01/27/2026  0:20:42.30 18683

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -46,12 +46,12 @@
 @endif
 
 @if(Auth::user()->role == 'admin')
-<li class="nav-item">
+{{-- <li class="nav-item">
     <a href="{{ route('dashboard.blogs') }}" class="nav-link {{ Request::is('dashboard/blogs') ? 'active' : '' }} {{ Request::is('dashboard/blogs/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-pen-nib"></i>
         <p>ব্লগ</p>
     </a>
-</li>
+</li> --}}
 @endif
 
 {{-- <li class="nav-item">
