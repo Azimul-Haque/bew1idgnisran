b0b7f63bf6c7f9f81e8ceeddbafa985fefba5a65
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 00:05:06 +0600

    Commit Tue 01/27/2026  0:05:06.26 25352

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -732,6 +732,21 @@ class DashboardController extends Controller
             ->header('Content-Disposition', 'attachment; filename="areas.json"');
     }
 
+    public function compareCenters()
+    {
+        // টেবিল থেকে প্রয়োজনীয় কলামগুলো নিয়ে আসা
+        $centers = \DB::table('center_list')
+                    ->select('id', 'code', 'name', 'name_detail')
+                    ->orderByRaw('CAST(id AS UNSIGNED) ASC') // আইডির ক্রমানুসারে সাজানো
+                    ->get();
+
+        return view('centers.compare', compact('centers'));
+    }
+
+
+
+
+
     // clear configs, routes and serve
     public function clear()
     {
