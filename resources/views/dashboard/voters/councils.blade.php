26910cda9ac26bf6c1e6c3ec91e5890602bb2add
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 01:02:01 +0600

    Commit Tue 01/27/2026  1:02:01.25 14079

--- resources/views/dashboard/voters/councils.blade.php
+++ resources/views/dashboard/voters/councils.blade.php
@@ -46,14 +46,6 @@
 </style>
 
 <div class="container-fluid">
-    <div class="row mb-2">
-        <div class="col-12">
-            <h3 class="font-weight-bold text-dark">
-                <i class="fas fa-map-marked-alt text-primary mr-2"></i> ইউনিয়ন/পৌরসভা ভিত্তিক ভোটার তথ্য
-            </h3>
-            <p class="text-muted">বিস্তারিত দেখতে নির্দিষ্ট প্রশাসনিক ইউনিটে ক্লিক করুন</p>
-        </div>
-    </div>
 
     <div class="row">
         @foreach($councils as $council)
