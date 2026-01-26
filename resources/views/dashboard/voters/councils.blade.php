d0c90d23a0df2001fdb3160ca7ec64d6dd02d41c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 01:12:55 +0600

    Commit Tue 01/27/2026  1:12:55.23 23990

--- resources/views/dashboard/voters/councils.blade.php
+++ resources/views/dashboard/voters/councils.blade.php
@@ -11,7 +11,7 @@
 @section('page-header-right')
   <ol class="breadcrumb float-sm-right">
       <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
-      <li class="breadcrumb-item active">ইউনিয়ন/পৌরসভা</li>
+      <li class="breadcrumb-item active">ইউনিয়ন-পৌরসভা</li>
   </ol>
 @endsection
 <style>
