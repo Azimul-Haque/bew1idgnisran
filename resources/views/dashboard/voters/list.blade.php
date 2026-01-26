fb6edf9081a45e59d8e33655f4c2c02ffbe8f270
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 00:50:51 +0600

    Commit Tue 01/27/2026  0:50:51.27 17183

---
+++ resources/views/dashboard/voters/list.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('title', 'ভোটার তালিকা')
+
+@section('content')
+<div class="container-fluid">
+    <div class="card card-primary card-outline shadow">
+        <div class="card-header">
+            <h3 class="card-title text-bold"><i class="fas fa-users"></i> {{ $area->name }} - বিস্তারিত ভোটার তালিকা</h3>
+            <div class="card-tools">
+                <span class="badge badge-info p-2">মোট ভোটার: {{ $voters->total() }}</span>
+            </div>
+        </div>
+        <div class="card-body p-0">
+            <table class="table table-striped table-hover m-0">
+                <thead class="bg-light">
+                    <tr>
+                        <th>ক্রমিক</th>
+                        <th>ভোটার আইডি</th>
+                        <th>নাম</th>
+                        <th>পিতার নাম</th>
+                        <th>লিঙ্গ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($voters as $voter)
+                    <tr>
+                        <td>{{ $voters->firstItem() + $loop->index }}</td>
+                        <td class="text-primary font-weight-bold">{{ $voter->voter_id }}</td>
+                        <td>{{ $voter->name }}</td>
+                        <td>{{ $voter->father_name }}</td>
+                        <td><span class="badge {{ $voter->gender == 'পুরুষ' ? 'badge-primary' : 'badge-danger' }}">{{ $voter->gender }}</span></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer clearfix">
+            <div class="float-right">
+                {{ $voters->links() }}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
