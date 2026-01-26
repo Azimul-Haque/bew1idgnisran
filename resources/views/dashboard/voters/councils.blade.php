d33c2ff8c6f5c44d6cc25e12fb3e4aaa2e8bc901
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 01:10:21 +0600

    Commit Tue 01/27/2026  1:10:21.22 21255

--- resources/views/dashboard/voters/councils.blade.php
+++ resources/views/dashboard/voters/councils.blade.php
@@ -69,7 +69,7 @@
             <a href="{{ url('voters/areas/'.$council->id) }}" class="text-decoration-none">
                 <div class="card council-card shadow-sm h-90 position-relative">
                     <i class="fas fa-city council-icon"></i>
-                    <div class="card-body p-4">
+                    <div class="card-body p-2">
                         <h5 class="font-weight-bold text-dark mb-3">{{ $council->name }}</h5>
                         
                         <div class="voter-stats d-flex justify-content-between align-items-center border-top pt-3 mt-2">
