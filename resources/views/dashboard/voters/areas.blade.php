697c932c2fd59a7d70b72bbeb427720c4d024061
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 01:29:25 +0600

    Commit Tue 01/27/2026  1:29:25.29 919

--- resources/views/dashboard/voters/areas.blade.php
+++ resources/views/dashboard/voters/areas.blade.php
@@ -79,7 +79,7 @@
                         {{ $area->name }}
                     </span>
 
-                    <div class="d-flex justify-content-center mb-2" style="font-size: 1rem; gap: 8px;">
+                    <div class="d-flex justify-content-center mb-2" style="font-size: 0.85rem; gap: 8px;">
                         <span class="text-muted">
                             পুরুষ: {{ bangla(number_format($area->male ?? 0)) }}
                         </span>
