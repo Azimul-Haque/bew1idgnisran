a6d54397ab5992fe44c887de191b5b6429526d5e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 00:50:06 +0600

    Commit Tue 01/27/2026  0:50:06.25 13161

--- resources/views/dashboard/voters/areas.blade.php
+++ resources/views/dashboard/voters/areas.blade.php
@@ -15,7 +15,7 @@
             <div class="card card-outline card-primary shadow-sm h-100 mb-3">
                 <div class="card-body text-center p-3">
                     <h5 class="card-title text-bold d-block mb-3">{{ $area->name }}</h5>
-                    <a href="{{ url('voters/list/'.$area->id) }}" class="btn btn-primary btn-block btn-sm">ভোটার তালিকা দেখুন</a>
+                    <a href="{{ route('dashboard.councils.voters', $area->id) }}" class="btn btn-primary btn-block btn-sm">ভোটার তালিকা দেখুন</a>
                 </div>
             </div>
         </div>
