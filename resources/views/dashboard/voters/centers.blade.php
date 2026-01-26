f83846ca9fd70737ff7e34141da533754c927892
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 00:06:42 +0600

    Commit Tue 01/27/2026  0:06:42.23 8799

--- resources/views/dashboard/voters/centers.blade.php
+++ resources/views/dashboard/voters/centers.blade.php
@@ -3,7 +3,6 @@
 
 @section('third_party_stylesheets')
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
 @endsection
 
 @section('content')
