16cc8f2068cbf004bd53ef483d55540bbed4a1a0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 00:26:54 +0600

    Commit Tue 01/27/2026  0:26:54.24 2058

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -48,7 +48,8 @@
 @if(Auth::user()->role == 'admin')
 <li class="nav-item">
     <a href="{{ route('dashboard.notifications') }}" class="nav-link {{ Request::is('dashboard/notifications') ? 'active' : '' }} {{ Request::is('dashboard/notification/*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-bell"></i>
+        <i class="nav-icon fas fa-person-booth"></i>
+        <i class=""></i>
         <p>ভোটকেন্দ্রসমূহ</p>
     </a>
 </li>
