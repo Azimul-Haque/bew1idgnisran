31ec35a834ec322bc34babbf8a0a36e4e8610214
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-27 01:18:35 +0600

    Commit Tue 01/27/2026  1:18:35.26 12440

--- resources/views/dashboard/voters/areas.blade.php
+++ resources/views/dashboard/voters/areas.blade.php
@@ -2,6 +2,12 @@
 @section('title', 'এলাকা নির্বাচন')
 
 @section('content')
+@section('page-header')
+    <h3 class="font-weight-bold text-dark">
+        <i class="fas fa-map-marked-alt text-primary mr-2"></i> ইউনিয়ন/পৌরসভা ভিত্তিক ভোটার তথ্য
+    </h3>
+    <p class="text-muted small">বিস্তারিত দেখতে নির্দিষ্ট প্রশাসনিক ইউনিটে ক্লিক করুন</p>
+@endsection
 @section('page-header-right')
   <ol class="breadcrumb float-sm-right">
       <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
