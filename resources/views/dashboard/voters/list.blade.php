037c30276189d698acf32b62756bb2644ba749cb
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-28 00:03:15 +0600

    Commit Wed 01/28/2026  0:03:15.31 8919

--- resources/views/dashboard/voters/list.blade.php
+++ resources/views/dashboard/voters/list.blade.php
@@ -13,6 +13,13 @@
         <div class="card-header">
             <h3 class="card-title text-bold"><i class="fas fa-users"></i> {{ $area->name }} - বিস্তারিত ভোটার তালিকা</h3>
             <div class="card-tools">
+                <div class="btn-group btn-group-sm mr-2">
+                    <a href="{{ request()->fullUrlWithQuery(['gender' => '']) }}" class="btn {{ request('gender') == '' ? 'btn-primary' : 'btn-default' }}">সব</a>
+                    <a href="{{ request()->fullUrlWithQuery(['gender' => 'পুরুষ']) }}" class="btn {{ request('gender') == 'পুরুষ' ? 'btn-primary' : 'btn-default' }}">পুরুষ</a>
+                    <a href="{{ request()->fullUrlWithQuery(['gender' => 'মহিলা']) }}" class="btn {{ request('gender') == 'মহিলা' ? 'btn-primary' : 'btn-default' }}">মহিলা</a>
+                    <a href="{{ request()->fullUrlWithQuery(['gender' => '৩য় লিঙ্গ']) }}" class="btn {{ request('gender') == '৩য় লিঙ্গ' ? 'btn-primary' : 'btn-default' }}">৩য় লিঙ্গ</a>
+                </div>
+                
                 <span class="badge badge-info p-2">মোট ভোটার: {{ $voters->total() }}</span>
             </div>
         </div>
@@ -20,7 +27,7 @@
             <table class="table table-bordered table-hover table-condensed table-hover m-0">
                 <thead class="bg-light">
                     <tr>
-                        <th>সিরিয়াল</th>
+                        <th>সিরিয়াল</th>
                         <th>ভোটার আইডি</th>
                         <th>নাম</th>
                         <th>জন্মতারিখ</th>
@@ -31,7 +38,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($voters as $voter)
+                    @forelse($voters as $voter)
                     <tr>
                         <td>{{ $voter->sl_no }}</td>
                         <td class="text-primary font-weight-bold">{{ $voter->voter_id }}</td>
@@ -40,15 +47,23 @@
                         <td>{{ $voter->profession }}</td>
                         <td>{{ $voter->father_name }}</td>
                         <td>{{ $voter->mother_name }}</td>
-                        <td><span class="badge {{ $voter->gender == 'পুরুষ' ? 'badge-primary' : 'badge-danger' }}">{{ $voter->gender }}</span></td>
+                        <td>
+                            <span class="badge {{ $voter->gender == 'পুরুষ' ? 'badge-primary' : ($voter->gender == 'মহিলা' ? 'badge-danger' : 'badge-warning') }}">
+                                {{ $voter->gender }}
+                            </span>
+                        </td>
                     </tr>
-                    @endforeach
+                    @empty
+                    <tr>
+                        <td colspan="8" class="text-center p-3">কোন ভোটার পাওয়া যায়নি।</td>
+                    </tr>
+                    @endforelse
                 </tbody>
             </table>
         </div>
         <div class="card-footer clearfix">
             <div class="float-left">
-                {{ $voters->links() }}
+                {{ $voters->appends(request()->query())->links() }}
             </div>
         </div>
     </div>
