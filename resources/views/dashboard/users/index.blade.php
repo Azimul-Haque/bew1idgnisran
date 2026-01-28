88f911283b3fe1b88352ab5b4e9d47cb3033decd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-29 02:15:05 +0600

    Commit Thu 01/29/2026  2:15:05.29 11663

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -225,17 +225,19 @@
                               <span aria-hidden="true">&times;</span>
                             </button>
                           </div>
-                          <div class="modal-body">
-                            আপনি কি নিশ্চিতভাবে এই ব্যবহারকারীকে এক্টিভেট করতে চান?<br/>
-                            <center>
-                                <big><b>{{ $user->name }}</b></big><br/>
-                                স্থানীয় সরকার কার্যালয়: {{ $user->localOffice != null ? $user->localOffice->name_bn : '' }}
-                            </center>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                            <a href="{{ route('dashboard.users.activate', $user->id) }}" class="btn btn-warning">এক্টিভেট করুন</a>
-                          </div>
+                          <form method="post" action="{{ route('dashboard.users.update', $user->id) }}">
+                            <div class="modal-body">
+                              আপনি কি নিশ্চিতভাবে এই ব্যবহারকারীকে এক্টিভেট করতে চান?<br/>
+                              <center>
+                                  <big><b>{{ $user->name }}</b></big><br/>
+                                  স্থানীয় সরকার কার্যালয়: {{ $user->localOffice != null ? $user->localOffice->name_bn : '' }}
+                              </center>
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                              <a href="{{ route('dashboard.users.activate', $user->id) }}" class="btn btn-warning">এক্টিভেট করুন</a>
+                            </div>
+                          </form>
                         </div>
                       </div>
                     </div>
