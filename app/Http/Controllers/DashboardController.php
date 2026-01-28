1d5e893e1e9db7edf7677e94370ef4c11b7e4bc7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-29 02:11:32 +0600

    Commit Thu 01/29/2026  2:11:32.28 22220

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -804,7 +804,7 @@ class DashboardController extends Controller
             ], 200);
 
             Session::flash('success', 'Blog Category updated successfully!');
-            return redirect()->route('dashboard.blogs');
+            return redirect()->back();
         }
 
         return response()->json([
