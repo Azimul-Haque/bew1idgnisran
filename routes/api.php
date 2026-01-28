45237500b0db5ea5811b452275955032bdb8f1a4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-29 01:34:54 +0600

    Commit Thu 01/29/2026  1:34:54.26 3809

--- routes/api.php
+++ routes/api.php
@@ -41,8 +41,8 @@ Route::get('/gallery/list', 'APIController@getGallery')->name('api.list.gallery'
 Route::post('/gallery/store', 'APIController@storeGallery')->name('api.store.gallery');
 Route::delete('/gallery/delete/{id}', 'APIController@deleteGallery')->name('api.delete.gallery');
 
-Route::get('/election/manifesto/bnp', 'APIController@getElectionMenifesto')->name('api.election.manifesto.bnp');
-Route::get('/election/manifesto/narsingdi-1', 'APIController@getElectionMenifesto')->name('api.election.manifesto.narsingdi-1');
+Route::get('/election/manifesto/bnp', 'APIController@getElectionMenifestoBNP')->name('api.election.manifesto.bnp');
+Route::get('/election/manifesto/narsingdi-1', 'APIController@getElectionMenifestoN1')->name('api.election.manifesto.narsingdi-1');
 
 Route::get('/voters', 'APIController@getVoters');
 
