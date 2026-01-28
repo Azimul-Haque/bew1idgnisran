63cbdf3a59320e7ffba39e28777d390befb5d718
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-29 02:18:01 +0600

    Commit Thu 01/29/2026  2:18:01.25 29276

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -232,7 +232,7 @@
                             </div>
                             <div class="modal-footer">
                               <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                              <button type="submit" class="btn btn-primary">আনলক করুন</button>
+                              <button type="submit" class="btn btn-warning">আনলক করুন</button>
                             </div>
                           </form>
                         </div>
